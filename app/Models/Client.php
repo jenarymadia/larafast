9050265fd970b0f24e5515b89b3855dabef2b353
Jenary Madia <jenary.madia@rebisco.com.ph>
2024-07-29 23:13:39 +0800

    initial commit for client

---
+++ app/Models/Client.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::creating(function ($client) {
+            $client->user_id = Auth::id();
+        });
+    }
+}
